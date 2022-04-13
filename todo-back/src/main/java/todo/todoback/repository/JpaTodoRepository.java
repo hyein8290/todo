4562114bf62031527438ee6dd325596b928f89f6
DESKTOP-08R6MMQ\user <hyein8290@gmail.com>
2022-04-13 17:52:11 +0900

    Add Comment

--- todo-back/src/main/java/todo/todoback/repository/JpaTodoRepository.java
+++ todo-back/src/main/java/todo/todoback/repository/JpaTodoRepository.java
@@ -14,6 +14,7 @@ public class JpaTodoRepository implements TodoRepository {
 
 	@Override
 	public List<Todo> findAll() {
+		// from절에 DB의 테이블이 아닌 entity명으로 조회해야 한다.
 		return em.createQuery("select t from Todo t", Todo.class).getResultList();
 	}
 
